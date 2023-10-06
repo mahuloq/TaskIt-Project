1cd6b8384b6672149248c1b0d1eeb6c6ad644057
mahuloq <95161346+mahuloq@users.noreply.github.com>
2023-10-05 20:24:53 -0500

    Finished Sidebar, started work on Task List

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {
+  userImage =
+    'https://ih1.redbubble.net/image.1362253646.0602/st,small,845x845-pad,1000x1000,f8f8f8.jpg';
 
+  userName = 'Matt';
+  userEmail = 'Matt@codingscool.com';
 }
